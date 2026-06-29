37e06fc8d114fb5df92e892b6f1bb528155175f5
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-06-29 22:33:46 +0500

    file explorer,sidebar and some more useful function are added

--- lua/core/plugins/editor.lua
+++ lua/core/plugins/editor.lua
@@ -1,1 +1,36 @@
-return {}
+
+return {
+
+    -- Auto close brackets
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
+
+    -- Surround words
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = true,
+    },
+
+    -- Visual undo tree
+    {
+        "mbbill/undotree",
+        keys = {
+            { "<leader>u", ":UndotreeToggle<CR>", desc = "Undo Tree" },
+        },
+    },
+
+    -- Zen mode
+    {
+        "folke/zen-mode.nvim",
+        keys = {
+            { "<leader>z", ":ZenMode<CR>", desc = "Zen Mode" },
+        },
+        config = true,
+    },
+
+}
--- lua/core/plugins/file_explorer.lua
+++ lua/core/plugins/file_explorer.lua
@@ -1,1 +1,82 @@
-return {}
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        keys = {
+            { "<C-f>", ":Neotree toggle dir=~<CR>", desc = "Toggle Explorer" },
+        },
+        config = function()
+            require("neo-tree").setup({
+                window = {
+                    width = 30,
+                    mappings = { ["<C-f>"] = "close_window" },
+                },
+                filesystem = {
+                    filtered_items = {
+                        hide_dotfiles = false,
+                        hide_gitignored = true,
+                    },
+                },
+                sources = { "filesystem", "buffers", "git_status" },
+                close_if_last_window = true,
+                default_component_configs = {
+                    icon = {
+                        folder_closed = "📁",
+                        folder_open = "📂",
+                        folder_empty = "📁",
+                        default = "📄",
+                        lua = "🌙",
+                        python = "🐍",
+                        javascript = "📜",
+                        typescript = "📘",
+                        html = "🌐",
+                        css = "🎨",
+                        json = "📋",
+                        yaml = "⚙️",
+                        md = "📝",
+                        txt = "📃",
+                        c = "⚡",
+                        cpp = "⚡",
+                        rust = "🦀",
+                        go = "🐹",
+                        java = "☕",
+                        sh = "💻",
+                        bash = "💻",
+                        zsh = "💻",
+                    },
+                    git_status = {
+                        symbols = {
+                            added = "+",
+                            modified = "~",
+                            deleted = "-",
+                            renamed = ">",
+                            untracked = "?",
+                            ignored = ".",
+                            unstaged = "!",
+                            staged = "✓",
+                            conflict = "⚠",
+                        },
+                    },
+                },
+            })
+        end,
+    },
+
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<leader>ff", ":Telescope find_files<CR>", desc = "Find files" },
+            { "<leader>fg", ":Telescope live_grep<CR>", desc = "Find text" },
+            { "<leader>fb", ":Telescope buffers<CR>", desc = "Buffers" },
+        },
+        config = function()
+            require("telescope").setup({})
+        end,
+    },
+}
