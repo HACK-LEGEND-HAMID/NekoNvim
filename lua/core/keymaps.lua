90d5aa781723bd13e4e4b943dfc88fa6ce282d90
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-01 21:15:58 +0500

    Live Preview Added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,3 +17,29 @@ map("n", "<C-r>", "<C-w>l")
 map("n", "ch", ":split<CR>", { desc = "Horizontal Split" })
 map("n", "cv", ":vsplit<CR>", { desc = "Vertical Split" })
 
+ -- Live Previw fot HTML
+local function html_preview()
+    local file = vim.fn.expand("%:p")
+    local dir = vim.fn.expand("%:p:h")
+    local fname = vim.fn.expand("%:t")
+    local os_name = vim.loop.os_uname().sysname
+
+    if os_name == "Linux" then
+        local is_termux = vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL
+        if is_termux then
+            vim.fn.system("pkill -f 'python3 -m http.server 8080' 2>/dev/null")
+            vim.fn.jobstart({ "python3", "-m", "http.server", "8080", "-d", dir }, { detach = true })
+            vim.fn.system("sleep 1")
+            vim.fn.jobstart({ "termux-open-url", "http://localhost:8080/" .. fname })
+        else
+            vim.fn.jobstart({ "firefox", file })
+        end
+    elseif os_name == "Darwin" then
+        vim.fn.jobstart({ "open", file })
+    elseif os_name == "Windows_NT" then
+        vim.fn.jobstart({ "cmd", "/c", "start", file })
+    end
+end
+
+map("n", "P", html_preview, { desc = "HTML Preview" })
+
