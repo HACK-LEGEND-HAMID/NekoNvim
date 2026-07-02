9cca9a00894571593ce1ac954bc9d72d355d051d
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-02 21:59:40 +0500

    keymap for boiler plate added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,29 +17,40 @@ map("n", "<C-r>", "<C-w>l")
 map("n", "ch", ":split<CR>", { desc = "Horizontal Split" })
 map("n", "cv", ":vsplit<CR>", { desc = "Vertical Split" })
 
- -- Live Previw fot HTML
+-- HTML Live Preview (Universal Python HTTP Server)
 local function html_preview()
     local file = vim.fn.expand("%:p")
     local dir = vim.fn.expand("%:p:h")
     local fname = vim.fn.expand("%:t")
     local os_name = vim.loop.os_uname().sysname
 
+    vim.fn.system("pkill -f 'python3 -m http.server 8080' 2>/dev/null")
+    vim.fn.jobstart(
+        { "python3", "-m", "http.server", "8080", "-d", dir },
+        { detach = true }
+    )
+
+    vim.fn.system("sleep 1")
     if os_name == "Linux" then
-        local is_termux = vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL
-        if is_termux then
-            vim.fn.system("pkill -f 'python3 -m http.server 8080' 2>/dev/null")
-            vim.fn.jobstart({ "python3", "-m", "http.server", "8080", "-d", dir }, { detach = true })
-            vim.fn.system("sleep 1")
+        if vim.fn.getenv("TERMUX_VERSION") ~= vim.NIL then
             vim.fn.jobstart({ "termux-open-url", "http://localhost:8080/" .. fname })
         else
-            vim.fn.jobstart({ "firefox", file })
+            vim.fn.jobstart({ "xdg-open", "http://localhost:8080/" .. fname })
         end
     elseif os_name == "Darwin" then
-        vim.fn.jobstart({ "open", file })
+        vim.fn.jobstart({ "open", "http://localhost:8080/" .. fname })
     elseif os_name == "Windows_NT" then
-        vim.fn.jobstart({ "cmd", "/c", "start", file })
+        vim.fn.jobstart({ "cmd", "/c", "start", "http://localhost:8080/" .. fname })
     end
 end
 
-map("n", "P", html_preview, { desc = "HTML Preview" })
+map("n", "<leader>P", html_preview, { desc = "HTML Preview" })
 
+
+-- Snippet expand with Ctrl+E
+vim.keymap.set({ "i", "s" }, "<C-e>", function()
+    local ls = require("luasnip")
+    if ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+    end
+end, { silent = true })
