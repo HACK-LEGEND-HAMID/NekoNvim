694233bcd7204b52f308cacde2dd41a86844f9b2
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-03 16:30:32 +0500

    Hide Error for Teemux

--- lua/core/plugins/ai.lua
+++ lua/core/plugins/ai.lua
@@ -2,5 +2,10 @@ return {
     {
         "Exafunction/codeium.vim",
         event = "BufEnter",
+        cond = function()
+            local ok, result = pcall(vim.fn.system, { "uname", "-m" })
+            if not ok then return false end
+            return result:find("x86_64") ~= nil
+        end,
     },
 }
