2085ba239118f90f3ff4a19e497be3b6ef4520ce
HACK-LEGEND-HAMID <droxsea@gmail.com>
2026-07-03 16:27:53 +0500

    Error Fixed in dashboard init.lua file

--- lua/core/dashboard/init.lua
+++ lua/core/dashboard/init.lua
@@ -25,7 +25,7 @@ function M.setup()
             { name = "🌙 Quit",          action = "qa",                          section = "Setup" },
         },
 
-        footer = footer = "🐱 With great power comes great responsibility \n",
+        footer = "🐱 With great power comes great responsibility \n",
 
         content_hooks = {
             starter.gen_hook.adding_bullet("  ", false),
